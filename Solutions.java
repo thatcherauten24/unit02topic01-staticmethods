d33833b0da2e1491e7ef473fd573d0ac8111a4fa
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-31 12:04:41 -0600

    1. max3 complete, starting 2. odd

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,31 @@
 public class Solutions {
-    public static void main(String[] args) {
+
+    public static int max3(int a, int b, int c) {
+        if(a > b && a > c) {
+            return a;
+        } else if(b > a && b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static double max3(double a, double b, double c) {
+        if(a > b && a > c) {
+            return a;
+        } else if(b > a && b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static boolean odd(boolean a, boolean b, boolean c) {
         
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(max3(4, 8, 3));
     }
 }
