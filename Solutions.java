bf2f2dd1ba02f1c649b368dce0f4583a8cc6bdf7
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-31 12:14:31 -0600

    2. odd completed

--- Solutions.java
+++ Solutions.java
@@ -21,11 +21,27 @@ public class Solutions {
     }
 
     public static boolean odd(boolean a, boolean b, boolean c) {
-        
+        int total = 0;
+        if(a) {
+            total = total + 1;
+        }
+        if(b) {
+            total = total + 1;
+        }
+        if(c) {
+            total = total + 1;
+        } else {
+            total = 0;
+        }
+        if(total % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
     public static void main(String[] args) {
-        System.out.println(max3(4, 8, 3));
+        System.out.println(odd(true, false, true));
     }
 }
